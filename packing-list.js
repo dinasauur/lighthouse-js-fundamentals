4eb7e9d02eb688beee616044565cfce38fc08fda
Wendina <wendina.teng@gmail.com>
2023-02-20 08:48:59 +0000

    Learning about looping through an array using for loop

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let list of packingList) {
+  console.log(list);
+}
+
+console.log(packingList[3]);
